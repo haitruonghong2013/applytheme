ca192e09fa942536ffe2cc8ac288532c17acfc7d
HaiTruong <haitruong@cnc.com.vn>
2013-10-10 10:24:54 +0700

    implement login using username or email, validates username functions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131009031855) do
+ActiveRecord::Schema.define(:version => 20131010021045) do
 
   create_table "client_notes", :force => true do |t|
     t.integer  "client_id"
@@ -112,11 +112,13 @@ ActiveRecord::Schema.define(:version => 20131009031855) do
     t.string   "name"
     t.string   "authentication_token"
     t.integer  "organization_id"
+    t.string   "username"
   end
 
   add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
   create_table "users_roles", :id => false, :force => true do |t|
     t.integer "user_id"
