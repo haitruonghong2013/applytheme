70f9bb71548dfa71e96642f7542aa909b21baef2
HaiTruong <haitruong@cnc.com.vn>
2013-10-14 16:32:30 +0700

    remove address field on client model

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,7 @@
 class Client < ActiveRecord::Base
-  has_one :location
+  has_one :location, :dependent => :destroy
   has_many :users, :through => :client_notes
   belongs_to :user,:foreign_key => 'created_by'
   accepts_nested_attributes_for :location
-  attr_accessible :location_attributes,:address, :age, :city, :country, :email, :name, :phone, :sex
+  attr_accessible :location_attributes, :age, :city, :country, :email, :name, :phone, :sex
 end
