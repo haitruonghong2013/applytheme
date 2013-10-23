9ccdf2f56bf071aea70b78ffc92e9a737cd1b06a
HaiTruong <haitruong@cnc.com.vn>
2013-10-23 10:04:20 +0700

    change login function view

--- config/application.rb
+++ config/application.rb
@@ -58,6 +58,7 @@ module Ipadapp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
     # Heroku requires this to be false
     config.assets.initialize_on_precompile=false
