24fe0d0a77cae7d158e3f454e79a1fd77abc73b4
HaiTruong <haitruong@cnc.com.vn>
2013-10-29 18:03:59 +0700

    update and fix some bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
   skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
   #protect_from_forgery
-  PAGE_SIZE = 10
+  PAGE_SIZE = 4
   #after_filter :store_location
 
 
